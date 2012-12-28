5f11c8ce5c0d451ca5eb1c362b21817a0277205f
bernalf <berfer88@gmail.com>
2012-12-27 18:00:28 -0600

    Aulas y escuelas estables

--- Source/FTMatricula/Models/Classroom.cs
+++ Source/FTMatricula/Models/Classroom.cs
@@ -11,6 +11,7 @@ namespace FTMatricula.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
     
     public partial class Classroom
     {
@@ -18,13 +19,25 @@ namespace FTMatricula.Models
         {
             this.Courses = new HashSet<Course>();
         }
-    
+        [ScaffoldColumn(false)]
         public System.Guid ClassroomID { get; set; }
+
+        [Display(Name = "Codigo Aula")]
         public string Code { get; set; }
+        
+        [ScaffoldColumn(false)]
         public Nullable<System.DateTime> InsertDate { get; set; }
+
+        [ScaffoldColumn(false)]
         public Nullable<System.Guid> InsertUserID { get; set; }
+
+        [ScaffoldColumn(false)]
         public Nullable<System.DateTime> ModifyDate { get; set; }
+
+        [ScaffoldColumn(false)]
         public Nullable<System.Guid> ModifyUserID { get; set; }
+
+        [ScaffoldColumn(false)]
         public string IpAddress { get; set; }
     
         public virtual ICollection<Course> Courses { get; set; }
