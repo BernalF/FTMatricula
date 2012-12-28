cb466b2ab8e69b28c84323df44b1846e0eae7fca
bernalf <berfer88@gmail.com>
2012-12-28 17:39:40 -0600

    Model login DONE with resource file

--- Source/FTMatricula/Models/AccountModels.cs
+++ Source/FTMatricula/Models/AccountModels.cs
@@ -3,77 +3,78 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Globalization;
 using System.Web.Security;
+using FTMatricula.Utilities.Helper;
+using System.ComponentModel;
 
 namespace FTMatricula.Models
 {
-
     public class ChangePasswordModel
     {
-        [Required]
+        [Required(ErrorMessageResourceType = typeof(ModelResources), ErrorMessageResourceName = "REQUIRED")]
         [DataType(DataType.Password)]
-        [Display(Name = "Current password")]
+        [Display(ResourceType = typeof(ModelResources), Name = "OLD_PASSWORD")]
         public string OldPassword { get; set; }
 
-        [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
-        [DataType(DataType.Password)]
-        [Display(Name = "New password")]
+        [Required(ErrorMessageResourceType = typeof(ModelResources), ErrorMessageResourceName = "REQUIRED")]
+        [StringLength(100, ErrorMessageResourceType = typeof(ModelResources), ErrorMessageResourceName = "PASS_STRING_LENGTH", MinimumLength = 6)]
+        [DataType(DataType.Password)]        
+        [Display(ResourceType = typeof(ModelResources), Name = "NEW_PASSWORD")]
         public string NewPassword { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        [Display( ResourceType = typeof(ModelResources), Name = "PASS_CONFIRM")]
+        [Compare("NewPassword", ErrorMessageResourceType = typeof(ModelResources), ErrorMessageResourceName = "PASS_CONFIRM_ERR")]
         public string ConfirmPassword { get; set; }
     }
 
     public class LoginModel
     {
-        [Required]
-        [Display(Name = "User name")]
+        [Required(ErrorMessageResourceType = typeof(ModelResources), ErrorMessageResourceName = "REQUIRED")]
+        [Display(ResourceType = typeof(ModelResources), Name = "USER")]
         public string UserName { get; set; }
 
-        [Required]
+        [Required(ErrorMessageResourceType = typeof(ModelResources), ErrorMessageResourceName = "REQUIRED")]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(ResourceType = typeof(ModelResources), Name = "PASSWORD")]
         public string Password { get; set; }
 
-        [Display(Name = "Remember me?")]
+        [Display(ResourceType = typeof(ModelResources), Name = "REMEMBER")]
         public bool RememberMe { get; set; }
     }
 
     public class RegisterModel
     {
-        [Required]
-        [Display(Name = "User name")]
+        [Required(ErrorMessageResourceType = typeof(ModelResources), ErrorMessageResourceName = "REQUIRED")]
+        [Display(ResourceType = typeof(ModelResources), Name = "USERNAME")]
         public string UserName { get; set; }
 
-        [Required]
+        [Required(ErrorMessageResourceType = typeof(ModelResources), ErrorMessageResourceName = "REQUIRED")]
         [DataType(DataType.EmailAddress)]
-        [Display(Name = "Email address")]
+        [Display(ResourceType = typeof(ModelResources), Name = "EMAIL")]
         public string Email { get; set; }
 
-        [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [Required(ErrorMessageResourceType = typeof(ModelResources), ErrorMessageResourceName = "REQUIRED")]
+        [StringLength(100, ErrorMessageResourceType = typeof(ModelResources), ErrorMessageResourceName = "PASS_STRING_LENGTH", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(ResourceType = typeof(ModelResources), Name = "PASSWORD")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Display(ResourceType = typeof(ModelResources), Name = "PASS_CONFIRM")]
+        [Compare("Password", ErrorMessageResourceType = typeof(ModelResources), ErrorMessageResourceName = "PASS_CONFIRM_ERR")]
         public string ConfirmPassword { get; set; }
     }
 
 
     public class UserModel
     {
-        [Display(Name = "User name")]
+        [Display(Name = "Usuario")]
         public string UserName { get; set; }
 
-        [Display(Name = "Status")]
+        [Display(Name = "Estado")]
         public string Status { get; set; }
 
-        [Display(Name = "E-mail")]
+        [Display(Name = "Correo Electronico")]
         public string Email { get; set; }
     }
 }
