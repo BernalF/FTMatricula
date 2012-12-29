0318cd6bfd9dd958ee1bc3938386b9485876555c
bernalf <berfer88@gmail.com>
2012-12-28 18:17:07 -0600

    register culturizable

--- Source/FTMatricula/Utilities/ModelResources.cs
+++ Source/FTMatricula/Utilities/ModelResources.cs
@@ -15,12 +15,14 @@ namespace FTMatricula.Utilities.Helper
         public static string CLASSROOM_MANT { get { return Resources.GetValue("CLASSROOM_MANT"); } }
         public static string CLEAN { get { return Resources.GetValue("CLEAN"); } }
         public static string CONTAINS { get { return Resources.GetValue("CONTAINS"); } }
+        public static string CURRENT_PASS { get { return Resources.GetValue("CURRENT_PASS"); } }
         public static string DELETE { get { return Resources.GetValue("DELETE"); } }
         public static string DELETE_CONFIRMATION { get { return Resources.GetValue("DELETE_CONFIRMATION"); } }
         public static string DESCRIPTION { get { return Resources.GetValue("DESCRIPTION"); } }
         public static string DISPLAY_GRID { get { return Resources.GetValue("DISPLAY_GRID"); } }
         public static string DOES_NOT_CONTAIN { get { return Resources.GetValue("DOES_NOT_CONTAIN"); } }
         public static string EDIT { get { return Resources.GetValue("EDIT"); } }
+        public static string EMAIL { get { return Resources.GetValue("EMAIL"); } }
         public static string ENDS_WITH { get { return Resources.GetValue("ENDS_WITH"); } }
         public static string FIND { get { return Resources.GetValue("FIND"); } }
         public static string FIND_VAL_WHAT { get { return Resources.GetValue("FIND_VAL_WHAT"); } }
@@ -33,18 +35,28 @@ namespace FTMatricula.Utilities.Helper
         public static string LOCATION_LINE2 { get { return Resources.GetValue("LOCATION_LINE2"); } }
         public static string LOCATION_MANT { get { return Resources.GetValue("LOCATION_MANT"); } }
         public static string LOCATION_NAME { get { return Resources.GetValue("LOCATION_NAME"); } }
+        public static string LOGIN_TITLE { get { return Resources.GetValue("LOGIN_TITLE"); } }
+        public static string NEW_PASSWORD { get { return Resources.GetValue("NEW_PASSWORD"); } }
         public static string NEXT_PAGE { get { return Resources.GetValue("NEXT_PAGE"); } }
         public static string NO_RECORDS { get { return Resources.GetValue("NO_RECORDS"); } }
         public static string OF_GRID { get { return Resources.GetValue("OF_GRID"); } }
+        public static string OLD_PASSWORD { get { return Resources.GetValue("OLD_PASSWORD"); } }
         public static string PAGE { get { return Resources.GetValue("PAGE"); } }
+        public static string PASS_CONFIRM { get { return Resources.GetValue("PASS_CONFIRM"); } }
+        public static string PASS_CONFIRM_ERR { get { return Resources.GetValue("PASS_CONFIRM_ERR"); } }
+        public static string PASS_STRING_LENGTH { get { return Resources.GetValue("PASS_STRING_LENGTH"); } }
+        public static string PASSWORD { get { return Resources.GetValue("PASSWORD"); } }
         public static string PREVIOUS_PAGE { get { return Resources.GetValue("PREVIOUS_PAGE"); } }
+        public static string REMEMBER { get { return Resources.GetValue("REMEMBER"); } }
+        public static string REQUIRED { get { return Resources.GetValue("REQUIRED"); } }
         public static string SCHOOL_CODE { get { return Resources.GetValue("SCHOOL_CODE"); } }
         public static string SCHOOL_MANT { get { return Resources.GetValue("SCHOOL_MANT"); } }
         public static string SCHOOL_NAME { get { return Resources.GetValue("SCHOOL_NAME"); } }
+        public static string SIGNIN { get { return Resources.GetValue("SIGNIN"); } }
         public static string STARTS_WITH { get { return Resources.GetValue("STARTS_WITH"); } }
-        public static string TEST_KEY_1 { get { return Resources.GetValue("TEST_KEY_1"); } }
-        public static string TEST_KEY_2 { get { return Resources.GetValue("TEST_KEY_2"); } }
         public static string UPDATE { get { return Resources.GetValue("UPDATE"); } }
+        public static string USER { get { return Resources.GetValue("USER"); } }
+        public static string USERNAME { get { return Resources.GetValue("USERNAME"); } }
 
 
     }
