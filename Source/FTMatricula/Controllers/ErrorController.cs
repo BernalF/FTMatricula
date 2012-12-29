63318c208502fedd9dff7a1a4a1e5bf9f9143eed
brleiva <brleiva@gmail.com>
2012-12-28 20:06:06 -0600

    Custom Error

---
+++ Source/FTMatricula/Controllers/ErrorController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FTMatricula.Controllers
+{
+    public class ErrorController : Controller
+    {
+        /// <summary>
+        ///  GET: /Error/
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        /// <summary>
+        ///  GET: /Error404/
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Error404()
+        {
+            return View();
+        }
+
+    }
+}
