518e51ddd938a72fce3323ae4b40fc917b0fbba3
bernalf <berfer88@gmail.com>
2013-03-02 13:00:54 -0600

    LinkPCController.cs

---
+++ Source/FTMatricula/Controllers/LinkPCController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FTMatricula.Models;
+
+namespace FTMatricula.Controllers
+{
+    public class LinkPCController : Controller
+    {
+        private matrifunDBEntities db = new matrifunDBEntities();
+
+        //
+        // GET: /LinkPC/
+
+        public ActionResult Index()
+        {            
+            return View();
+        }
+
+        
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
