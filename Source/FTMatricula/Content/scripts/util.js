38c3c47529bac262438570816d8d05af1f8ffa54
bernalf <berfer88@gmail.com>
2013-03-02 19:35:27 -0600

    interface link plan course

--- Source/FTMatricula/Content/scripts/util.js
+++ Source/FTMatricula/Content/scripts/util.js
@@ -106,6 +106,53 @@ function defaultErrorCallback (jqXHR, textStatus){
     });                      
 })(jQuery);
 
+/*
+* Author: Bernal Fernandez Rojas
+* 
+* Description: Filtered Generic Function to a ul list
+* 
+* Creation Date: February, 2013
+* 
+*/
+function filteredList (filtertxtID, containerID) {
+    (function ($) {
+        // custom css expression for a case-insensitive contains()
+        jQuery.expr[':'].Contains = function(a,i,m){
+            return (a.textContent || a.innerText || "").toUpperCase().indexOf(m[3].toUpperCase())>=0;
+        };
+                
+        function listFilter(list) { // header is any element, list is an unordered list
+            // create and add the filter form to the header
+            // var form = $("<form>").attr({"class":"filterform","action":"#"}),
+            // input = $("<input>").attr({"class":"filterinput","type":"text"});
+            //$(form).append(input).appendTo(header);
+            $("#"+filtertxtID+"").change( function () {
+                var filter = $(this).val();
+                if(filter) {
+                    // this finds all links in a list that contain the input,
+                    // and hide the ones not containing the input while showing the ones that do
+                    $(list).find('span:not(:Contains("'+ filter + '"))').slideUp();
+                    $(list).find("span:Contains(" + filter + ")").slideDown();
+                } else {
+                    $(list).find("span").slideDown();
+                }
+                
+              
+                return false;
+            });
+            $("#"+filtertxtID+"").keyup( function () {
+                // fire the above change event after every letter
+                $(this).change();
+
+            }); 
+        }
+        //ondomready
+        $(function () {
+            listFilter($("#"+containerID+""));
+        });
+    }(jQuery));       
+}
+
 
 /*
 * Author: Bernal Fernandez Rojas
