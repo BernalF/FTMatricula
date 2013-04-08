af5066f391dad6f3ec8e1149a34a986202fe13a8
bernalf <berfer88@gmail.com>
2013-04-07 22:37:37 -0600

    score

---
+++ Source/FTMatricula/Scripts/scoreRegister.js
@@ -0,0 +1,42 @@
+﻿/*
+* Score Register Javascript Class
+*/
+var scoreRegister = new Class({
+    Implements: [Options],
+    options: {},
+    //Initialize Function
+    initialize: function (options) {
+        this.setOptions(options);
+        this.PageLoad();
+    },
+    //Decides which function load according to the View
+    PageLoad: function () {
+        switch (this.options.view) {            
+            case 'INDEX':
+                //this.fillGroupGrid();                
+                break;
+        }
+    },
+    //fill the Grid Details Group 
+    fillGroupGrid: function (data) {
+        var self = this;
+        $("#groups_grid").kendoGrid({
+            dataSource: {
+                data: data                
+            },
+            columns: [{
+                field: "StudentID",
+                hidden: true
+            }, {
+                field: "Name",
+                title: "Nombre"
+            }, {
+                field: "LastName",
+                title: "Apellido"
+            }, {
+                field: "Score",
+                title: "Nota"
+            }]            
+        });
+    }
+});
