57063aebb28b889eb677a68c260bcae3e4b84c66
bernalf <berfer88@gmail.com>
2013-05-04 22:08:27 -0600

    Certifications V.O001

---
+++ Source/FTMatricula/Controllers/CertificationsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FTMatricula.Controllers
+{
+    public class CertificationsController : Controller
+    {
+        //
+        // GET: /Certifications/
+        public ActionResult index()
+        {
+            return View();
+        }
+
+    }
+}
