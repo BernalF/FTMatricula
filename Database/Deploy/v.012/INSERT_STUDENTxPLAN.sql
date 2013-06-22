7277e6c3bb016e3d792c29aadf50022d46545338
brleiva <brleiva@gmail.com>
2013-06-22 16:26:16 -0600

    insert de estudiante x plan

---
+++ Database/Deploy/v.012/INSERT_STUDENTxPLAN.sql
@@ -0,0 +1,7 @@
+USE matriFunDB
+GO
+
+INSERT StudentPlan (StudentID,PlanID,InsertDate)
+SELECT b.StudentID, a.PlanID, GETDATE()
+FROM Enrollment  a INNER JOIN EnrollmentStudent b  ON a.EnrollmentID = b.EnrollmentID
+GROUP BY b.StudentID, a.planID
